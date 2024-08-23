f6892b4a8e7af8f2f349c63d77a8355bfeeed966
Ygor Kappaun <ygorfingkapp@gmail.com>
2024-08-22 21:37:49 -0300

    adicionando os arquivos de login

---
+++ assets/JS/app.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    const container1 = document.getElementById('button');
+    const container2 = document.getElementById('animação');
+
+    container1.classList.remove('hidden');
+    container1.classList.add('show');
+    container2.classList.remove('animação');
+}, 1500);
