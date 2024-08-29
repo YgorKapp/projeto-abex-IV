f0e4f0eb4df08662a93dd29bd1bf643275caec9c
Pedrogmschabarum <147007746+Pedrogmschabarum@users.noreply.github.com>
2024-08-29 10:28:10 -0300

    Update Conexao.php

--- conexao/Conexao.php
+++ conexao/Conexao.php
@@ -4,10 +4,10 @@ class Conexao{
 
 	function criarConexao() {
 		try { 
-		    $host = "";     //configuracoes locais
-			$username = "";		
-			$password = "";
-			$db_name = "";
+		    $host = "http://onebus.cx8kmic8cryz.us-east-2.rds.amazonaws.com/";   //18.216.171.234
+			$username = "admin";		
+			$password = "onebus123";
+			$db_name = "onebus";
 
 			$con = mysqli_connect ( "$host", "$username", "$password" ) or die ( "cannot connect; "+mysqli_error($con) );
 			mysqli_select_db ( $con, "$db_name" ) or die ( "cannot select DB; "+mysqli_error($con) );
